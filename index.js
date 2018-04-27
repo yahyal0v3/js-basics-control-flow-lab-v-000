842db04986de5574f438373ad154f0298c5f96eb
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-04-27 19:04:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(feet) {
+  let greeting
+  if (feet <= 400) {
+    greeting = "This one is on me!"
+  } else if (feet > 2000 && feet <= 2500) {
+    greeting = "I will gladly take your thirty bucks."
+  } else if (feet > 2500) {
+    greeting = "No can do."
+  }
+  return greeting
+}
+
+function ternaryCheckCity(city) {
+  let response
+  if (city === "NYC") {
+    response = "Ok, sounds good."
+  }
+  return response
+}
